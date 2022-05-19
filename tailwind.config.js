9d9ad17bd55508f78d368469738abd7af82e5c99
Amanda Yusuf Pradana <amandayusufpradanaa@gmail.com>
2022-05-19 12:38:12 +0800

    hbd anida

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        oleo: ["Oleo Script Swash Caps"],
+        poppins: ["Poppins"],
+      },
+    },
+  },
+  plugins: [],
+};
